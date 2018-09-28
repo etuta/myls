6e5cb81a5b30e06ec886f227fe5a4c7e0f8908fb
tendol <tenzindolma98@gmail.com>
2018-09-28 00:15:42 -0400

    made some changes to the file direcotry path reading

--- myls.c
+++ myls.c
@@ -45,6 +45,7 @@ int diropt[256];
 int info_opt[256];
 int j;
 
+
 /*print permission*/
 void
 mode_to_letter(int mode, char* str);
@@ -61,74 +62,62 @@ information(void);
 int
 main(int argc, char*argv[])
 {
-  /*for(int i = 0; i < argc; i++)
+
+  opterr=0;
+
+
+  while((opt=getopt(argc, argv, "al"))!=-1)
   {
-  if(strncmp(argv[i],"-",1)==0)
-  {
-  option(argc, argv);
-  //printf("use_a %d", use_a);
-  //printf("use_l %d", use_l);
-}
-else {
-if(i==0)
-current_dir=".";
-else
-{
-current_dir=argv[i];
-j++;
-if(j>1)
-{
-printf("%s \n", current_dir);
-}
-
-}
-
-}
-}
-*/
-
-
-//if(flag==1){ */
-opterr=0;
-
-while((opt=getopt(argc, argv, "al"))!=-1)
-{
-  switch(opt)
-  {
-    case 'a':
-    use_a = 1;
-    break;
-    case 'l':
-    use_l=1;
-    break;
-    case '?':
-    printf("Option -%c is not an accepted argument.\n", optopt);
-    exit(3);
+    switch(opt)
+    {
+      case 'a':
+      use_a = 1;
+      break;
+      case 'l':
+      use_l=1;
+      break;
+      case '?':
+      printf("Option -%c is not an accepted argument.\n", optopt);
+      exit(3);
+    }
   }
+
+if(argv[optind]==NULL){
+  printf("what is in %d = NULL YES\n", optind );
+  root = opendir(current_dir);
+  if(root==NULL)
+  {
+    perror("opendir");
+    exit(1);
+  }
+
+  else
+  {
+    information();
+  }
+
 }
 
 
-/*  if(dir_flag==1){
-root=opendir(name);
-}
-else{
-root = opendir(current_dir);}*/
+  for(int i = optind; i<argc; i++){
+    printf("this is optind %d\n", optind);
+    printf("what is in %d = %s\n", optind,argv[optind]);
 
-root = opendir(current_dir);
-if(root==NULL)
-{
-  perror("opendir");
-  exit(1);
-}
+   root = opendir(argv[optind]);
+    if(root==NULL)
+    {
+      perror("opendir");
+      exit(1);
+    }
 
-/* reads the files in the directory*/
-else
-{
-  information();
-}
+      information();
 
-/* close directory */
-closedir(root);
+
+    optind++;
+  }
+
+  /* close directory */
+  closedir(root);
 }
 
 void
@@ -136,121 +125,42 @@ mode_to_letter(int mode, char *str)
 {
   str[0] = '-';
   // judge file type
-  if(S_ISDIR(mode))
-  {
-    str[0] = 'd';
-  }
-  if(S_ISCHR(mode))
-  {
-    str[0] = 'c';
-  }
-  if(S_ISBLK(mode))
-  {
-    str[0] = 'b';
-  }
+  if(S_ISDIR(mode)){ str[0] = 'd';}
+  if(S_ISCHR(mode)){ str[0] = 'c';}
+  if(S_ISBLK(mode)){ str[0] = 'b';}
 
   // judge permission for owner
-  if(mode & S_IRUSR)
-  {
-    str[1] = 'r';
-  }
-  else
-  {
-    str[1] = '-';
-  }
-  if(mode & S_IWUSR)
-  {
-    str[2] = 'w';
-  }
-  else
-  {
-    str[2] = '-';
-  }
-  if(mode & S_IXUSR)
-  {
-    str[3] = 'x';
-  }
-  else
-  {
-    str[3] = '-';
-  }
+  if(mode & S_IRUSR){ str[1] = 'r';}
+  else { str[1] = '-';}
+
+  if(mode & S_IWUSR){ str[2] = 'w';}
+  else { str[2] = '-';}
+
+  if(mode & S_IXUSR){ str[3] = 'x';}
+  else { str[3] = '-';}
 
   // judge permission for owner group
-  if(mode & S_IRGRP)
-  {
-    str[4] = 'r';
-  }
-  else
-  {
-    str[4] = '-';
-  }
-  if(mode & S_IWGRP)
-  {
-    str[5] = 'w';
-  }
-  else
-  {
-    str[5] = '-';
-  }
-  if(mode & S_IXGRP)
-  {
-    str[6] = 'x';
-  }
-  else
-  {
-    str[6] = '-';
-  }
+  if(mode & S_IRGRP){ str[4] = 'r';}
+  else{ str[4] = '-';}
+
+  if(mode & S_IWGRP){ str[5] = 'w';}
+  else{ str[5] = '-';}
+
+  if(mode & S_IXGRP){ str[6] = 'x';}
+  else{ str[6] = '-';}
 
   // judge permission for others
-  if(mode & S_IROTH)
-  {
-    str[7] = 'r';
-  }
-  else
-  {
-    str[7] = '-';
-  }
-  if(mode & S_IWOTH)
-  {
-    str[8] = 'w';
-  }
-  else
-  {
-    str[8] = '-';
-  }
-  if(mode & S_IXOTH)
-  {
-    str[9] = 'x';
-  }
-  else
-  {
-    str[9] = '-';
-  }
+  if(mode & S_IROTH){ str[7] = 'r';}
+  else{ str[7] = '-';}
+
+  if(mode & S_IWOTH){ str[8] = 'w';}
+  else{ str[8] = '-';}
+
+  if(mode & S_IXOTH){ str[9] = 'x';}
+  else{ str[9] = '-';}
 
   str[10] = '\0';
 }
-
-/*int
-option(int argc, char*argv[])
-{
-opterr=0;
-while((opt=getopt(argc, argv, "al"))!=-1)
-{
-switch(opt)
-{
-case 'a':
-use_a = 1;
-break;
-case 'l':
-use_l=1;
-break;
-case '?':
-printf("Option -%c is not an accepted argument.\n", optopt);
-exit(3);
-}
-}
-return(use_a ,use_l);
-}*/
 
 void
 information(void)
@@ -304,10 +214,7 @@ information(void)
         strftime(datestring, sizeof(datestring),"%b %d %H:%M", tm);
         printf(" %s %s\n", datestring, pDirent->d_name);
       }
-
     }
-
-
 
     else
     {
