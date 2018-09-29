b028809e789e544da3c6569bac2a36d5dec1b151
tendol <tenzindolma98@gmail.com>
2018-09-28 22:51:54 -0400

    debugging -l for directories

--- myls.c
+++ myls.c
@@ -37,7 +37,10 @@ char file_name[256];
 char* dir_name[256];
 char* current_dir=".";
 char* name;
+struct stat buffer;
 
+int current = 0;
+int status;
 int use_a =0;
 int use_l=0;
 int opt,x=0,y=0;
@@ -48,15 +51,11 @@ int j;
 
 /*print permission*/
 void
-mode_to_letter(int mode, char* str);
+mode_to_letter(mode_t mode, char* str);
 
 /*print permission and other info*/
 void
-information(void);
-
-/* seperate -l and -a from directories*/
-//int
-//option(int argc, char*argv[]);
+options(void);
 
 /*the main function*/
 int
@@ -82,8 +81,11 @@ main(int argc, char*argv[])
     }
   }
 
+  printf("this is the optind %d\n\n\n", optind);
+
 if(argv[optind]==NULL){
-  printf("what is in %d = NULL YES\n", optind );
+  //printf("what is in %d = NULL YES\n", optind );
+  current =1;
   root = opendir(current_dir);
   if(root==NULL)
   {
@@ -93,7 +95,7 @@ if(argv[optind]==NULL){
 
   else
   {
-    information();
+    options();
   }
 
 }
@@ -102,17 +104,16 @@ if(argv[optind]==NULL){
   for(int i = optind; i<argc; i++){
     printf("this is optind %d\n", optind);
     printf("what is in %d = %s\n", optind,argv[optind]);
-
-   root = opendir(argv[optind]);
+current_dir = argv[optind];
+   root = opendir(current_dir);
     if(root==NULL)
     {
       perror("opendir");
       exit(1);
     }
-
-      information();
-
-
+    else{
+      options();
+    }
     optind++;
   }
 
@@ -121,69 +122,110 @@ if(argv[optind]==NULL){
 }
 
 void
-mode_to_letter(int mode, char *str)
+mode_to_letter(mode_t mode, char *str)
 {
+  //printf("is the permission being printed\n");
   str[0] = '-';
   // judge file type
+  printf("directory %x \n",S_ISDIR(mode) );
   if(S_ISDIR(mode)){ str[0] = 'd';}
+  printf("c %x \n",S_ISCHR(mode));
   if(S_ISCHR(mode)){ str[0] = 'c';}
+  printf("b %x \n",S_ISBLK(mode) );
   if(S_ISBLK(mode)){ str[0] = 'b';}
 
   // judge permission for owner
-  if(mode & S_IRUSR){ str[1] = 'r';}
+  printf("mode= %d S_IRUSR = %d \n", mode, S_IRUSR);
+  printf("mode & sth = %d \n", mode & S_IRUSR );
+  if((mode & S_IRUSR)== S_IRUSR) { str[1] = 'r';}
   else { str[1] = '-';}
 
-  if(mode & S_IWUSR){ str[2] = 'w';}
+  printf("mode= %d S_IWUSR = %d \n", mode, S_IWUSR);
+  printf("mode & sth = %d \n", mode & S_IWUSR);
+  if((mode & S_IWUSR) == S_IWUSR){ str[2] = 'w';}
   else { str[2] = '-';}
 
-  if(mode & S_IXUSR){ str[3] = 'x';}
+  printf("mode= %d S_IXUSR = %d \n", mode, S_IXUSR);
+  printf("mode & sth = %d \n", mode & S_IXUSR);
+  if((mode & S_IXUSR) ==S_IXUSR ){ str[3] = 'x';}
   else { str[3] = '-';}
 
+  printf("mode= %d S_IRGRP = %d \n", mode, S_IRGRP);
+  printf("mode & sth = %d \n", mode & S_IRGRP);
   // judge permission for owner group
-  if(mode & S_IRGRP){ str[4] = 'r';}
+  if((mode & S_IRGRP) ==S_IRGRP){ str[4] = 'r';}
   else{ str[4] = '-';}
 
-  if(mode & S_IWGRP){ str[5] = 'w';}
+  printf("mode= %d S_IWGRP = %d \n", mode, S_IWGRP);
+  printf("mode & sth = %d \n", mode &S_IWGRP);
+  if((mode & S_IWGRP) == S_IWGRP){ str[5] = 'w';}
   else{ str[5] = '-';}
 
-  if(mode & S_IXGRP){ str[6] = 'x';}
+  printf("mode= %d S_IXGRP = %d \n", mode, S_IXGRP);
+  printf("mode & sth = %d \n", mode & S_IXGRP);
+  if((mode & S_IXGRP) ==S_IXGRP){ str[6] = 'x';}
   else{ str[6] = '-';}
 
+  printf("mode= %d S_IROTH = %d \n", mode, S_IROTH);
+  printf("mode & sth = %d \n", mode & S_IROTH);
   // judge permission for others
-  if(mode & S_IROTH){ str[7] = 'r';}
+  if((mode & S_IROTH) == S_IROTH){ str[7] = 'r';}
   else{ str[7] = '-';}
 
-  if(mode & S_IWOTH){ str[8] = 'w';}
+  printf("mode= %d S_IWOTH= %d \n", mode, S_IWOTH);
+  printf("mode & sth = %d \n", mode & S_IWOTH);
+  if((mode & S_IWOTH) ==S_IWOTH){ str[8] = 'w';}
   else{ str[8] = '-';}
 
-  if(mode & S_IXOTH){ str[9] = 'x';}
+  printf("mode= %d S_IXOTH = %d \n", mode, S_IXOTH);
+  printf("mode & sth = %d \n", mode & S_IXOTH);
+  if((mode & S_IXOTH) == S_IXOTH ){ str[9] = 'x';}
   else{ str[9] = '-';}
 
   str[10] = '\0';
 }
 
 void
-information(void)
+options(void)
 {
   while((pDirent = readdir(root))!=NULL)
   {
     strncpy(file_name, pDirent->d_name, 256);
     if((file_name[0]==46))
     {
+      //printf("\nthe file has a flag \n");
       if(use_a==1 && use_l!=1)
       {
+        //printf("\nOnly -a no -l\n");
         printf ("%s \n", pDirent->d_name);
       }
       else if(use_l==1 && use_a==1)
       {
-        if(stat(pDirent->d_name, &statbuf) == -1)
-        continue;
-
+        //status = stat("/home", &buffer);
+        //printf("the status of stat = %d \n", status);
+      //  printf("\n-l and -a\n");
+    // printf("\nsomething 2 = %d \n",stat(pDirent->d_name, &statbuf) );
+    if(current == 1){
+     if(stat(pDirent->d_name, &statbuf) == -1){
+    //   perror("stat");
+     continue;
+   }
+ }
+ else {
+   if(stat(current_dir, &statbuf) == -1){
+  //   perror("stat");
+   continue;
+ }
+ }
         /* print permissions */
+        printf("the stat.mode = %d\n", statbuf.st_mode);
+
         mode_to_letter(statbuf.st_mode,str);
+      //  printf("%10.10s", sperm (statbuf.st_mode));
+          //    printf("%4d", statbuf.st_nlink);
+
         //file type and permissions
         printf("%s ",str);
-
         printf("%ld ", statbuf.st_nlink);
 
         /* print out owner's name */
@@ -195,7 +237,6 @@ information(void)
         {
           printf("%d ", statbuf.st_uid);
         }
-
         /*Print out group name*/
         if((grp = getgrgid(statbuf.st_gid))!=NULL)
         {
@@ -205,10 +246,8 @@ information(void)
         {
           printf("%d ", statbuf.st_gid);
         }
-
         /*Print out fileq size*/
         printf("%6d ", (int)statbuf.st_size);
-
         /*Print the local time*/
         tm = localtime(&statbuf.st_mtime);
         strftime(datestring, sizeof(datestring),"%b %d %H:%M", tm);
@@ -220,16 +259,39 @@ information(void)
     {
       if(use_l==1)
       {
-        if(stat(pDirent->d_name, &statbuf) == -1)
-        continue;
+        //printf("\nonly -l \n");
+      //  printf("the file name %s \n", pDirent->d_name);
+       //printf("\nsomething %d \n",stat(pDirent->d_name, &statbuf) );
+       //printf("\n this is different %d \n", stat(current_dir, &statbuf));
+       printf("the name of the file = %s \n", pDirent->d_name);
 
+
+      if(current == 1){
+       if(stat(pDirent->d_name, &statbuf) == -1){
+         printf("okay something is wrong with 1");
+      //   perror("stat");
+       continue;
+     }
+   }
+   else {
+     if(stat(current_dir, &statbuf) == -1){
+        printf("okay something is wrong with 2");
+    //   perror("stat");
+     continue;
+   }
+   }
+        //printf("something is being printed here\n");
         /* print permissions */
+          printf("the stat.mode = %d\n", statbuf.st_mode);
+
+          //mode_to_letter(statbuf.st_mode,str);
         mode_to_letter(statbuf.st_mode,str);
+      //  printf("%10.10s", sperm (statbuf.st_mode));
+        //      printf("%4d", statbuf.st_nlink);
+
         //file type and permissions
         printf("%s ",str);
-
         printf("%ld ", statbuf.st_nlink);
-
         /* print out owner's name */
         if((pwd = getpwuid(statbuf.st_uid))!= NULL)
         {
@@ -239,7 +301,6 @@ information(void)
         {
           printf("%d ", statbuf.st_uid);
         }
-
         /*Print out group name*/
         if((grp = getgrgid(statbuf.st_gid))!=NULL)
         {
@@ -249,10 +310,8 @@ information(void)
         {
           printf("%d ", statbuf.st_gid);
         }
-
         /*Print out fileq size*/
         printf("%6d ", (int)statbuf.st_size);
-
         /*Print the local time*/
         tm = localtime(&statbuf.st_mtime);
         strftime(datestring, sizeof(datestring),"%b %d %H:%M", tm);
